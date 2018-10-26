7e0de3b06b4abea1907a9f0e2a1a72016fa7ca05
diegomlopes <diegomenezeslopes@icloud.com>
2018-10-26 16:12:04 -0400

    Initial Commit

---
+++ ListWithTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListWithTableView
+//
+//  Created by Diego Lopes on 26/10/18.
+//  Copyright © 2018 Diego Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
